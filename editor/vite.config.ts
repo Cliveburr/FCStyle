c39e2fbbb01c1106854d23c0af3d515b284ac46b
Clivedurr <clivedurr@gmail.com>
2025-08-22 12:38:50 -0300

    update docs

--- editor/vite.config.ts
+++ editor/vite.config.ts
@@ -1,20 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command, mode }) => ({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src'
-    }
+      '@': '/src',
+    },
   },
   css: {
     preprocessorOptions: {
       scss: {
         // Remover o additionalData para evitar conflitos com @use/@import
         // O import será feito manualmente no arquivo SCSS
-      }
-    }
-  }
-})
+      },
+    },
+  },
+  // Define o "base" apenas no momento do build
+  base: command === 'build' ? '/fcstyle/docs/' : '/',
+}));
